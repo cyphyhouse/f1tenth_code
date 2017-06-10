93dc8c565423bfe180a484dada5bb1e0f59fd7fa
tanyax <tanyav2@illinois.edu>
2017-06-10 18:31:55 -0500

    moved file

--- imu_odometry/odometer.py
+++ imu_odometry/odometer.py
@@ -3,8 +3,9 @@
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Imu
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import Twist
 
+global pub
 
 # Publishes the current data on the topic curr_pos
 def publish_position(pose_data):
@@ -15,31 +16,31 @@ def publish_position(pose_data):
 # Subscribes node to the topic /imu which has msgs of type 
 # sensor_msgs/Imu messages being published on it via the imu
 def get_imu_messages():
-	rospy.Subscriber('imu', sensor_msgs.msgs.Imu, get_position)
+	rospy.Subscriber('imu0', Imu, get_position)
 	rospy.sleep(0.5)
 	rospy.spin()
 
 
 # Uses the equations of motion to get position of the robot
-def get_position():
-	pose_data = geometry_msgs.msgs.PoseWithCovarianceStamped()
+def get_position(imu_data):
+	pose_data = Twist()
 	
 	# the time difference between receiving two msgs
 	rate = 0.5	
 
 	# s = ut + 1/2at^2
 	# to get linear velocity, we use linear acceleration
-	ux = Imu.linear_acceleration_covariance[0]*rate
-	uy = Imu.linear_acceleration_covariance[1]*rate
-	pose_data[0] = ux*rate + 0.5*Imu.linear_acceleration_covariance[0]*rate*rate
-	pose_data[1] = uy*rate + 0.5*Imu.linear_acceleration_covariance[1]*rate*rate
-	pose_data[2] = 0
+	ux = imu_data.linear_acceleration.x*rate
+	uy = imu_data.linear_acceleration.y*rate
+	pose_data.linear.x = ux*rate + 0.5*imu_data.linear_acceleration.x*rate*rate
+	pose_data.linear.y = uy*rate + 0.5*imu_data.linear_acceleration.y*rate*rate
+	pose_data.linear.z = 0
 	
 	# Magnetometer needs to be incorporated here to measure turn 
       
-        pose_data[3] = Imu.angular_velocity[0]*rate
-      	pose_data[4] = Imu.angular_velocity[1]*rate
-      	pose_data[5] = 0
+        pose_data.angular.x = imu_data.angular_velocity.x*rate
+      	pose_data.angular.y = imu_data.angular_velocity.y*rate
+      	pose_data.angular.z = 0
 	
 	publish_position(pose_data)
 	
@@ -50,8 +51,8 @@ if __name__ == '__main__':
 		
 		# Initializing the publisher, all the odometry data will be
 		# published on the topic curr_pos 
-		global pub = rospy.Publisher('curr_pos', 
-				geometry_msgs.msgs.PoseWithCovarianceStamped, queue_size=1000)
+		pub = rospy.Publisher('curr_pos', 
+				Twist, queue_size=1000)
 		
 		get_imu_messages()
 	except rospy.ROSInterruptException:
